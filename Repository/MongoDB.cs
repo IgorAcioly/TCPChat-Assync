76c53c0f82374f5d0e979e7c2afef1f6ec185ef0
Igor <igor.g.acioly@gmail.com>
2025-05-09 01:59:48 -0300

    Feature: Instalando dependências do MongoDB

---
+++ Repository/MongoDB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration.Internal;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace TCPChat_Assync.Repository
+{
+    class MongoDB
+    {
+
+        public IMongoDatabase DB { get; } 
+
+        public MongoDB(IConfiguration configuration) 
+        {
+            try
+            {
+                var client = new MongoClient(configuration["mongodb + srv://igor:12345@clusteraps.iyptrun.mongodb.net/?retryWrites=true&w=majority&appName=ClusterAPS"]);
+                DB = client.GetDatabase(configuration["ChatTCP"]);
+                MapClasses();
+
+            }
+            catch (Exception ex)
+            {
+                throw new MongoException("Não foi possível acessar o banco",ex);
+            
+            }    
+        }
+
+
+
+    }
+}
